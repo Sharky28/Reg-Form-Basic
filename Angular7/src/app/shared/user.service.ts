c50929249de78a90639cc82d8097621575a93808
SharkyUgas <sharmarke.ugas@outlook.com>
2019-06-20 12:24:45 +0100

    added validation along with error methods

--- Angular7/src/app/shared/user.service.ts
+++ Angular7/src/app/shared/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,26 @@ export class UserService {
 
 
   formModel = this.fb.group({
-    UserName:[''],
-    Email:[''],
+    UserName:['',Validators.required],
+    Email:['',Validators.email],
     FullName:[''],
     Passwords : this.fb.group({
-      Password:[''],
-      ConfirmPassword:['']     
-    })
+      Password:['',[Validators.required,Validators.minLength(4)]],
+      ConfirmPassword:['',Validators.required]     
+    },{validator: this.comparePasswords })
 
-  })
+  });
+
+  comparePasswords(fb:FormGroup){
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPswrdCtrl.errors ={passwordMismatcg:true}
+    if(confirmPswrdCtrl.errors==null || 'passwordMismatch' in confirmPswrdCtrl.errors){
+      if(fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({passwordMismatch:true});
+        else
+        confirmPswrdCtrl.setErrors(null);
+    }
+
+  }
 }
